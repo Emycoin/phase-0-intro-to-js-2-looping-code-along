ca9f50ed2456e53e5e22e7c252dbe5153241245e
Emycoin <emytausevich@gmail.com>
2021-06-21 14:52:55 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    const namesArr = [];
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  
+  namesArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+  }
+  return namesArr;
+}
+function countDown(integer){
+  while(integer >= 0){
+    console.log(integer);
+    integer--;
+  } 
+}
+
